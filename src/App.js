02098367b5ec3a7fcdb228b76b40e2e583cba058
dianamorenodev <valentinamoreno2002@gmail.com>
2023-02-14 11:55:21 -0500

    correccion de componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.scss';
 import Container from './components/Container';
-import FormTodo from './components/FormTodo';
-import TaskList from './components/TaskList'
 
 function App() {
   return (
     <div className="App">
       <Container />
-      <FormTodo />
-      <TaskList />
     </div>
   );
 }
